461f8fc5be90a13253a8749a65b54e76da3a8cdf
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-07-04 10:52:51 -0300

    :sparkles: feat: created rabbitMQConfig

---
+++ src/main/java/com/example/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,52 @@
+package com.example.rabbitmq.config;
+
+import com.example.rabbitmq.listener.OrderMessageListener;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "orderQueue";
+    public static final String EXCHANGE_NAME = "orderExchange";
+    public static final String ROUTING_KEY = "orderRoutingKey";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                                    MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(OrderMessageListener listener) {
+        return new MessageListenerAdapter(listener, "receiveMessage");
+    }
+}
